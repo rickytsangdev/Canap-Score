33414050e9cc5b9663173f01b9418249ae9496ff
rickytsangdev <rickytsang.dev@gmail.com>
2024-03-02 20:23:50 +0100

    intialize kanap-foot project with react

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import Navbar from "./components/navbar";
+
+function App() {
+	return (
+		<div class=" bg-red-600">
+			<Navbar />
+		</div>
+	);
+}
+
+export default App;
---
+++ client/src/components/navbar.js
@@ -0,0 +1,11 @@
+export default function Navbar() {
+	return (
+		<>
+			<div className="navbar bg-base-100" data-theme="cyberpunk">
+				<a href className="btn btn-ghost text-xl">
+					KANAP⚽FOOT
+				</a>
+			</div>
+		</>
+	);
+}
